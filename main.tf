203038f3d160b5b9704c1f9b0360f04f6e157ac1
Marko <makimitrovic07@gmail.com>
2023-09-25 10:37:14 +0200

    feature/devops-26 - Naming resources using Microsoft naming convention

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 variable "resource_group" {}
 variable "location" {}
 
-resource "azurerm_mssql_server" "example" {
-  name                         = "devops-mssql-server"
+resource "azurerm_mssql_server" "sql-automation-dev-neu-01" {
+  name                         = "sql-automation-dev-neu-01"
   resource_group_name          = var.resource_group
   location = var.location
   version                      = "12.0"
@@ -10,12 +10,12 @@ resource "azurerm_mssql_server" "example" {
   administrator_login_password = "TzLKisoNPk3e!"
 }
 
-resource "azurerm_mssql_database" "example" {
-  name                = "devops-mssql-database"
+resource "azurerm_mssql_database" "sqldb-automation-dev-neu-01" {
+  name                = "sqldb-automation-dev-neu-01"
   sku_name     = "Basic"
   collation           = "SQL_Latin1_General_CP1_CI_AS"
   max_size_gb         = 1
-  server_id = azurerm_mssql_server.example.id
+  server_id = azurerm_mssql_server.sql-automation-dev-neu-01.id
 
   tags = {
     environment = "development"
