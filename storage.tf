c4870f40d6d55b0cd4b18fc1a0e40edd3e7210f2
DamjanVincic <damjanvincic2003@gmail.com>
2023-09-25 14:12:00 +0200

    Added more variables

--- storage.tf
+++ storage.tf
@@ -3,14 +3,14 @@ data "azurerm_resource_group" "devops_rg" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "st${var.app_name}01"
+  name                     = "st${lower(var.app_name)}${var.environment}01"
   resource_group_name      = data.azurerm_resource_group.devops_rg.name
   location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = var.account_tier
+  account_replication_type = var.replication_type
 }
 
 resource "azurerm_storage_container" "storage_container" {
-  name                 = "sc-${var.app_name}-${var.environment}-${var.location}-01"
+  name                 = "sc-${lower(var.app_name)}-${var.environment}-${var.location}-01"
   storage_account_name = azurerm_storage_account.storage_account.name
 }
--- variables.tf
+++ variables.tf
@@ -3,28 +3,43 @@ variable "location" {
     description = "location where zour resource needs provision in azure"
     default = "northeurope"
 }
+
 variable "resource_group" {
     type = string
     description = "resource_group name"
     default = "DevOps"
 }
+
 variable "app_name" {
     type = string
     description = "Name of Application"
     default = "PlanePal"
 }
+
 variable "environment" {
     type = string
     description = "Name of Environment"
     default = "dev"
 }
+
 variable "dot_net_version" {
     type = string
     description = "Dot Net Version"
     default = "v6.0"
 }
+
 variable "app_sku" {
     type = string
     description = "Plan for application"
     default = "F1"
+}
+
+variable "account_tier" {
+    type = string
+    default = "Standard"
+}
+
+variable "replication_type" {
+    type = string
+    default = "LRS"
 }
