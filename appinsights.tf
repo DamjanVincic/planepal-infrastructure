e9bc054f557942087e149782de27d79f9be3d87e
DamjanVincic <damjanvincic2003@gmail.com>
2023-09-25 12:59:57 +0200

    Changed app insights to use a name made from variables

--- appinsights.tf
+++ appinsights.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "devops_rg" {
 }
 
 resource "azurerm_application_insights" "app_insights" {
-  name                = "appi-planepal-dev-northeurope-01"
+  name                = "appi-${var.app_name}-${var.environment}-${var.location}-01"
   resource_group_name = data.azurerm_resource_group.devops_rg.name
   location            = var.location
   application_type    = "web"
