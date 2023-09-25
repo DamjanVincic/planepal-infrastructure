03a6d4754127283163644abc042d686c575ef7c7
Marko <makimitrovic07@gmail.com>
2023-09-25 13:18:23 +0200

    feature/devops-26 - Moved resources creation to separated file and added more variables

--- variables.tf
+++ variables.tf
@@ -1,21 +1,45 @@
 variable "location" {
-      type = string
+    type = string
+    description = "location where zour resource needs provision in azure"
+    default = "northeurope"
 }
 variable "resource_group" {
-      type = string
+    type = string
+    description = "resource_group name"
+    default = "DevOps"
 }
-variable "application_name" {
-      type = string
+variable "app_name" {
+    type = string
+    description = "Name of Application"
+    default = "PlanePal"
 }
-variable "enviornment" {
-      type = string
+variable "environment" {
+    type = string
+    description = "Name of Environment"
+    default = "dev"
 }
 variable "location_abbreviation" {
       type = string
+      default = "neu"
 }
 variable "sql_login" {
       type = string
+      default = "admin"
 }
 variable "sql_password" {
       type = string
+      default = "admin"
+}
+variable "sql_version" {
+      type = string
+      default = "12.0"
+}
+variable "sqldb_sku_name" {
+      type = string
+      default= "basic"
+}
+
+variable "sqldb_sku_max_gb_size" {
+      type = number
+      default = 1
 }
