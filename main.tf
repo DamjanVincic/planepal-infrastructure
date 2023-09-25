b124d48e83e12b753308fc5ed7127fe028184bfa
DamjanVincic <damjanvincic2003@gmail.com>
2023-09-25 10:31:59 +0200

    Added storage account and container for application

--- main.tf
+++ main.tf
@@ -1,2 +1,16 @@
-variable "resource_group" {}
-variable "location" {}
+data "azurerm_resource_group" "devops_rg" {
+  name = var.resource_group
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "stplanepal01"
+  resource_group_name      = data.azurerm_resource_group.devops_rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                 = "sc-planepal-dev-northeurope-01"
+  storage_account_name = azurerm_storage_account.storage_account.name
+}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
 variable "location" {
-    type = string
-    description = "location where zour resource needs provision in azure"
-    default = "North Europe"
+  type    = string
+  default = "North Europe"
+}
+
+variable "resource_group" {
+  type    = string
+  default = "DevOps"
 }
