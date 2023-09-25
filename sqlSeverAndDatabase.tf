e85584f984d4592d06c65cf45ed99a73db76c4c5
Marko <makimitrovic07@gmail.com>
2023-09-25 14:14:46 +0200

    feature/devops-26 - Fixing found typos in words

--- sqlSeverAndDatabase.tf
+++ sqlSeverAndDatabase.tf
@@ -1,5 +1,5 @@
 resource "azurerm_mssql_server" "sql-planepal-dev-neu-01" {
-  name                         = "sql-${var.app_name}-${var.enviornment}-${var.location_abbreviation}-00"
+  name                         = "sql-${var.app_name}-${var.environment}-${var.location_abbreviation}-00"
   resource_group_name          = var.resource_group
   location = var.location
   version                      = var.sql_version
@@ -8,7 +8,7 @@ resource "azurerm_mssql_server" "sql-planepal-dev-neu-01" {
 }
 
 resource "azurerm_mssql_database" "sqldb-planepal-dev-neu-01" {
-  name                = "sqldb-${var.app_name}-${var.enviornment}-${var.location_abbreviation}-00"
+  name                = "sqldb-${var.app_name}-${var.environment}-${var.location_abbreviation}-00"
   sku_name     = var.sqldb_sku_name
   collation           = "SQL_Latin1_General_CP1_CI_AS"
   max_size_gb         = var.sqldb_sku_max_gb_size
