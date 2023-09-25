7acfd2f649234e1b63433bf1e03b1c493a31e586
Mihajlo Jankovic <m.jankovic-int@levi9.com>
2023-09-25 12:25:40 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -18,6 +18,16 @@ variable "environment" {
     description = "Name of Environment"
     default = "dev"
 }
+variable "dot_net_version" {
+    type = string
+    description = "Dot Net Version"
+    default = "v6.0"
+}
+variable "app_sku" {
+    type = string
+    description = "Plan for application"
+    default = "F1"
+}
 variable "location_abbreviation" {
       type = string
       default = "neu"
