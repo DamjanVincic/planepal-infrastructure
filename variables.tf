be3faa4b89f435d66afc8b722e2237e05b6cd018
Marko <makimitrovic07@gmail.com>
2023-09-25 13:49:49 +0200

    feature/devops-26 - Added variables for SQL server and databases

--- variables.tf
+++ variables.tf
@@ -28,3 +28,53 @@ variable "app_sku" {
     description = "Plan for application"
     default = "F1"
 }
+variable "location_abbreviation" {
+      type = string
+      default = "neu"
+}
+variable "sql_login" {
+      type = string
+      default = "admin"
+}
+variable "sql_password" {
+      type = string
+      default = "admin"
+}
+variable "sql_version" {
+      type = string
+      default = "12.0"
+}
+variable "sqldb_sku_name" {
+      type = string
+      default= "basic"
+}
+
+variable "sqldb_sku_max_gb_size" {
+      type = number
+      default = 1
+}
+variable "resource_group" {
+    type = string
+    description = "resource_group name"
+    default = "DevOps"
+}
+variable "app_name" {
+    type = string
+    description = "Name of Application"
+    default = "PlanePal"
+}
+variable "environment" {
+    type = string
+    description = "Name of Environment"
+    default = "dev"
+}
+variable "dot_net_version" {
+    type = string
+    description = "Dot Net Version"
+    default = "v6.0"
+}
+variable "app_sku" {
+    type = string
+    description = "Plan for application"
+    default = "F1"
+}
