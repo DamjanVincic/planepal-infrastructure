358ce3ac729805829adf521ba3ff2b40106d7347
Irena Filipovic <i.filipovic-int@levi9.com>
2023-09-25 13:29:38 +0200

    f

--- variables.tf
+++ variables.tf
@@ -1,5 +1,30 @@
-variable "kv_app_sku_name" {
+variable "location" {
     type = string
-    description = "sku name for app key vault"
-    default = "standard"
+    description = "location where zour resource needs provision in azure"
+    default = "northeurope"
+}
+variable "resource_group" {
+    type = string
+    description = "resource_group name"
+    default = "DevOps"
+}
+variable "app_name" {
+    type = string
+    description = "Name of Application"
+    default = "PlanePal"
+}
+variable "environment" {
+    type = string
+    description = "Name of Environment"
+    default = "dev"
+}
+variable "dot_net_version" {
+    type = string
+    description = "Dot Net Version"
+    default = "v6.0"
+}
+variable "app_sku" {
+    type = string
+    description = "Plan for application"
+    default = "F1"
 }
