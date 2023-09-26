5dd207cba84b43b5b2ef4dc7578c5373c93a0049
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2023-09-26 14:43:05 +0200

    Update output.tf

--- modules/appservice/output.tf
+++ modules/appservice/output.tf
@@ -1,6 +1,9 @@
-output "web_app_instance" {
-  value       = azurerm_windows_web_app.app-PlanePal-dev-northeurope-00.identity[0]
+output "tenant_id" {
+  value = azurerm_windows_web_app.app-PlanePal-dev-northeurope-00.identity[0].tenant_id
+}
+output "object_id" {
+  value = azurerm_windows_web_app.app-PlanePal-dev-northeurope-00.identity[0].principal_id
 }
 output "web_app_id" {
-  value       = azurerm_windows_web_app.app-PlanePal-dev-northeurope-00.id
+  value = azurerm_windows_web_app.app-PlanePal-dev-northeurope-00.id
 }
