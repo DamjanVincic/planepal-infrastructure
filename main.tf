abf80652c2151cf42f24003a5516ca624a9c2612
Irena Filipovic <i.filipovic-int@levi9.com>
2023-09-26 14:58:25 +0200

    added key_vault module to main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ provider "azurerm" {
   skip_provider_registration = true
   features {}
 }
+
+module "key_vault" {
+  source = "./modules/keyvault"
+
+  location = var.location
+  resource_group = var.resource_group
+  app_name = var.app_name
+  environment = var.environment
+  kv_app_sku_name = var.kv_app_sku_name
+  tenant_id = var.tenant_id
+  principal_id = module.app-service.object_id
+    
+  
+}
