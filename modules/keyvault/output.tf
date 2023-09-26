04bfc40b3f42e151905909be5901d36499dbf974
Irena Filipovic <i.filipovic-int@levi9.com>
2023-09-26 14:28:37 +0200

    small fix

--- modules/keyvault/output.tf
+++ modules/keyvault/output.tf
@@ -1,9 +1,9 @@
 output "id" {
-  value       = azurerm_key_vault.main.id
+  value       = azurerm_key_vault.kv_for_app.id
   description = "The ID of the Key Vault."
 }
 
 output "name" {
-  value       = azurerm_key_vault.main.name
+  value       = azurerm_key_vault.kv_for_app.name
   description = "The name of the Key Vault."
 }
