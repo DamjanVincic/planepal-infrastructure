9a1524b381342e5266335f26ce150a34381c2056
Irena Filipovic <i.filipovic-int@levi9.com>
2023-09-27 13:57:02 +0200

    added default values for secrets

--- variables.tf
+++ variables.tf
@@ -95,16 +95,20 @@ variable "key_sql_password" {
 
 variable "kv_base_URL" {
   type        = string
+  default     = "http://api.aviationstack.com/v1/"
 }
 
 variable "kv_API_key" {
   type        = string
+  default     = "80a59692084fa732aa86028061a0d769"
 }
 
 variable "kv_email" {
   type        = string
+  default     = "yourplainpal@gmail.com"
 }
 
 variable "kv_email_pass" {
   type        = string
+  default     = "zboi ogmk minq lrya"
 }
