4412164d696308c99b1247082bcf2af125c82661
Irena Filipovic <i.filipovic-int@levi9.com>
2023-10-03 12:15:00 +0000

    removed dns zone and dns zone network link from network.tf

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -47,15 +47,4 @@ resource "azurerm_subnet" "az_subnet" {
   enforce_private_link_endpoint_network_policies = true
 }
 
-resource "azurerm_private_dns_zone" "az_dns_zone" {
-  name                = "planePal.com"
-  resource_group_name = var.resource_group_name
-}
 
-resource "azurerm_private_dns_zone_virtual_network_link" "az_virtual_network_link" {
-  name                  = "${azurerm_private_dns_zone.az_dns_zone.name}-link"
-  resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.az_dns_zone.name
-  virtual_network_id    = azurerm_virtual_network.az_vNet.id
-  registration_enabled  = false
-}
