e4e499ed6dd2c63789a96e1eac085826ddd597f8
Mihajlo Jankovic <m.jankovic-int@levi9.com>
2023-10-03 11:22:51 +0000

    Updated appservice.tf

--- modules/appservice/appservice.tf
+++ modules/appservice/appservice.tf
@@ -75,6 +75,25 @@ resource "azurerm_windows_web_app" "app-PlanePal-dev-northeurope-00" {
   }
 }
 
+resource "azurerm_private_endpoint" "private-ep-app-service" {
+  name                = "private-ep-app-service"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = module.network.subnet["subnet_app_service"].id
+  private_service_connection {
+    name                    = "azurerm_app_service_virtual_network_swift_connection"
+    private_connection_type = "ServiceEndpoint"
+    subresource_names       = ["appservice"]
+    is_manual_connection    = false
+  }
+}
+resource "azurerm_app_service_virtual_network_swift_connection" "az_vNet" {
+  app_service_id              = azurerm_windows_web_app.app-PlanePal-dev-northeurope-00.id
+  virtual_network_subnet_ids  = module.network.subnet["subnet_app"].id
+  swift_enabled               = true
+}
+
+
 # resource "azurerm_monitor_autoscale_setting" "scale_action_setting" {
 #   name                = "app-scale-${var.app_name}-${var.environment}-${var.location}-00"
 #   resource_group_name = var.resource_group_name
