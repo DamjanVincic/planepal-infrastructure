925946be495b8755e9534d8b583981fbb57416c0
Irena Filipovic <i.filipovic-int@levi9.com>
2023-10-05 13:57:14 +0200

    fix

--- modules/appservice/appservice.tf
+++ modules/appservice/appservice.tf
@@ -153,7 +153,7 @@ resource "azurerm_network_security_group" "nsg_app" {
     direction                  = "Inbound"
     source_port_range          = "*"
     destination_port_ranges    = [443]
-    source_address_prefix      = var.vm_source_address # get from vm
+    source_address_prefix      = var.vm_ip
     destination_address_prefix = "*"
   }
 
