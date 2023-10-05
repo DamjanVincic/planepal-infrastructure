474e76744f0c07b8fef4beb3522529eceaa646bd
DamjanVincic <damjanvincic2003@gmail.com>
2023-10-05 15:02:05 +0200

    Fixed vm OS disk storage account type

--- modules/vm/vm.tf
+++ modules/vm/vm.tf
@@ -62,7 +62,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_LRS"
   }
 
   source_image_reference {
